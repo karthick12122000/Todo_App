0588dbda9f9425cb5c14781b9923ec661c41a3a5
Karthick <103041319+karthick12122000@users.noreply.github.com>
2023-11-15 09:08:50 +0530

    Feat: Added Access-Control-Allow-Origin

--- server.js
+++ server.js
@@ -5,7 +5,15 @@ require("dotenv").config();
 var port = process.env.PORT || 3000;
 
 const cors = require("cors");
-app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://karthick12122000.github.io"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 app.use(express.json());
 
